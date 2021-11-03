e6166bc4ad73b2efac908036494ed88c5d361e9a
LCoder <loidv@midota.com>
2021-11-03 15:49:05 +0700

    update

--- src/navigator/BottomContainer.js
+++ src/navigator/BottomContainer.js
@@ -13,7 +13,7 @@ const BottomContainer = () => {
         name={router.HOME_SCREEN}
         component={bottom[router.HOME_SCREEN]}
         options={{
-          tabBarLabel: 'Home',
+          tabBarLabel: 'Trang chủ',
           tabBarIcon: ({color, size}) => (
             <MaterialCommunityIcons name="home" color={color} size={size} />
           ),
@@ -22,18 +22,50 @@ const BottomContainer = () => {
       <Tab.Screen
         name={router.SHOPPING_SCREEN}
         component={bottom[router.SHOPPING_SCREEN]}
+        options={{
+          tabBarLabel: 'Mua sắm',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="shopping" color={color} size={size} />
+          ),
+        }}
       />
       <Tab.Screen
         name={router.QR_CODE_SCREEN}
         component={bottom[router.QR_CODE_SCREEN]}
+        options={{
+          tabBarLabel: 'Qr code',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="qrcode-scan"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
       />
       <Tab.Screen
         name={router.VOUCHER_SCREEN}
         component={bottom[router.VOUCHER_SCREEN]}
+        options={{
+          tabBarLabel: 'Voucher',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="vote" color={color} size={size} />
+          ),
+        }}
       />
       <Tab.Screen
         name={router.PROFILE_SCREEN}
         component={bottom[router.PROFILE_SCREEN]}
+        options={{
+          tabBarLabel: 'Khác',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="dots-horizontal"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
