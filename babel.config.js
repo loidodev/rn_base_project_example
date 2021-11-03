89164aeca41d9d33abd9f4a04bfc179750978a6f
LCoder <loidv@midota.com>
2021-11-03 14:17:48 +0700

    update

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@contant': './src/contant',
+          '@hooks': './src/hooks',
+          '@navigator': './src/navigator',
+          '@screens': './src/screens',
+          '@store': './src/store',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
