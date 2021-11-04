baa49a7215322d4dd2c31e03d878b118a96df4c8
LCoder <loidv@midota.com>
2021-11-04 11:24:08 +0700

    update

--- src/screens/bottom/Profile/index.js
+++ src/screens/bottom/Profile/index.js
@@ -1,11 +1,11 @@
+import {Block, HeaderSearch} from '@components';
 import React from 'react';
-import {View, Text} from 'react-native';
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <Block flex>
+      <HeaderSearch title="Thông tin khác" />
+    </Block>
   );
 };
 
