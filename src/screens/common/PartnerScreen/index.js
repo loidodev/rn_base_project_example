639fbf84353153c14483fffd042266d1777f0be6
hoa pham <phamgiaohoa2000@gmail.com>
2021-11-09 12:33:14 +0700

    PartnerScreen and BirthdayScreen

---
+++ src/screens/common/PartnerScreen/index.js
@@ -0,0 +1,17 @@
+import {Block, HeaderSearch, WebView} from '@components';
+import React from 'react';
+
+const PartnerScreen = value => {
+  const {title} = value.route.params.params;
+
+  return (
+    <Block flex>
+      <HeaderSearch canGoBack title={title} />
+      <Block flex padding={12} paddingBottom={20}>
+        <WebView data={'https://topshare.vn/hinh-anh-buon-co-don-dep/'} />
+      </Block>
+    </Block>
+  );
+};
+
+export default PartnerScreen;
