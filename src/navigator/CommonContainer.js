8b0972fc68e889b333c3d32b20b5253717cda363
LCoder <loidv@midota.com>
2021-11-09 14:27:57 +0700

    update

--- src/navigator/CommonContainer.js
+++ src/navigator/CommonContainer.js
@@ -1,22 +1,26 @@
+/* eslint-disable react-native/no-inline-styles */
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {common} from '@screens';
 import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import router from './router';
 
 const CommonStack = createNativeStackNavigator();
 
 const CommonContainer = () => {
   return (
-    <CommonStack.Navigator screenOptions={{headerShown: false}}>
-      <CommonStack.Screen
-        name={router.GET_START_SCREEN}
-        component={common[router.GET_START_SCREEN]}
-      />
-      <CommonStack.Screen
-        name={router.GET_SEARCH_SCREEN}
-        component={common[router.GET_SEARCH_SCREEN]}
-      />
-    </CommonStack.Navigator>
+    <SafeAreaView style={{flex: 1}}>
+      <CommonStack.Navigator screenOptions={{headerShown: false}}>
+        <CommonStack.Screen
+          name={router.GET_START_SCREEN}
+          component={common[router.GET_START_SCREEN]}
+        />
+        <CommonStack.Screen
+          name={router.GET_SEARCH_SCREEN}
+          component={common[router.GET_SEARCH_SCREEN]}
+        />
+      </CommonStack.Navigator>
+    </SafeAreaView>
   );
 };
 
