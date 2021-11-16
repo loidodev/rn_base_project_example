e42374a36d063e67dd50095202616667419796f4
LCoder <loidv@midota.com>
2021-11-16 17:01:48 +0700

    update

--- src/screens/bottom/Profile/index.js
+++ src/screens/bottom/Profile/index.js
@@ -1,6 +1,7 @@
 import {Block} from '@components';
 import {commonRoot} from '@navigator/navigationRef';
 import {vs} from '@responsive';
+import router from '@router';
 import React, {useState} from 'react';
 import {RefreshControl} from 'react-native';
 import Animated, {
@@ -9,6 +10,7 @@ import Animated, {
 } from 'react-native-reanimated';
 import AvatarPicker from './components/AvatarPicker';
 import BannerHeader from './components/BannerHeader';
+import BoxLogin from './components/BoxLogin';
 import BtnLogout from './components/BtnLogout';
 import {GENERAL_LIST, MANAGER_LIST, SUPPORT_LIST} from './components/data';
 import Delivery from './components/Delivery';
@@ -20,11 +22,11 @@ import {
 import Information from './components/Information';
 import ListProfile from './components/ListProfile';
 import ShareAndReferredCode from './components/ShareAndReferredCode';
-import router from '@router';
 
 const Profile = () => {
   const scrollY = useSharedValue(0);
   const [isPickerAvatar, setIsPickerAvatar] = useState(false);
+  const user = false;
 
   const _onScroll = useAnimatedScrollHandler(event => {
     scrollY.value = event.contentOffset.y;
@@ -36,43 +38,47 @@ const Profile = () => {
 
   const _onOpenPickerAvatar = () => setIsPickerAvatar(true);
 
-  return (
-    <Block flex>
-      {/* banner */}
-      <BannerHeader scrollY={scrollY} />
-      {/* content */}
-      <Block flex safeAreaTop>
-        <Animated.ScrollView
-          onScroll={_onScroll}
-          showsVerticalScrollIndicator={false}
-          refreshControl={<RefreshControl refreshing={false} />}>
-          <Block height={HEIGHT_BG_WAVE} />
-          <Block
-            style={{
-              height: vs(HEIGHT_AVATAR) + vs(HEIGHT_BOX_INFO),
-              marginTop: vs(-HEIGHT_AVATAR / 2),
-            }}
+  if (!user) {
+    return <BoxLogin />;
+  } else {
+    return (
+      <Block flex>
+        {/* banner */}
+        <BannerHeader scrollY={scrollY} />
+        {/* content */}
+        <Block flex safeAreaTop>
+          <Animated.ScrollView
+            onScroll={_onScroll}
+            showsVerticalScrollIndicator={false}
+            refreshControl={<RefreshControl refreshing={false} />}>
+            <Block height={HEIGHT_BG_WAVE} />
+            <Block
+              style={{
+                height: vs(HEIGHT_AVATAR) + vs(HEIGHT_BOX_INFO),
+                marginTop: vs(-HEIGHT_AVATAR / 2),
+              }}
+            />
+            <Delivery />
+            <ListProfile delay={550} data={MANAGER_LIST} />
+            <ListProfile delay={600} data={GENERAL_LIST} />
+            <ListProfile delay={650} data={SUPPORT_LIST} />
+            <ShareAndReferredCode delay={700} />
+            <BtnLogout />
+          </Animated.ScrollView>
+          <Information
+            scrollY={scrollY}
+            onEditUser={_onEditUser}
+            onPickerAvatar={_onOpenPickerAvatar}
           />
-          <Delivery />
-          <ListProfile delay={550} data={MANAGER_LIST} />
-          <ListProfile delay={600} data={GENERAL_LIST} />
-          <ListProfile delay={650} data={SUPPORT_LIST} />
-          <ShareAndReferredCode delay={700} />
-          <BtnLogout />
-        </Animated.ScrollView>
-        <Information
-          scrollY={scrollY}
-          onEditUser={_onEditUser}
-          onPickerAvatar={_onOpenPickerAvatar}
+        </Block>
+        {/* modal */}
+        <AvatarPicker
+          isVisible={isPickerAvatar}
+          setIsVisible={setIsPickerAvatar}
         />
       </Block>
-      {/* modal */}
-      <AvatarPicker
-        isVisible={isPickerAvatar}
-        setIsVisible={setIsPickerAvatar}
-      />
-    </Block>
-  );
+    );
+  }
 };
 
 export default Profile;
