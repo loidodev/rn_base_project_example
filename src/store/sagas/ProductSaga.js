7449bac0cdf297bde9d0a99ce168d86e83e24934
hoa pham <phamgiaohoa2000@gmail.com>
2021-11-18 16:01:35 +0700

    fix

--- src/store/sagas/ProductSaga.js
+++ src/store/sagas/ProductSaga.js
@@ -1,11 +1,11 @@
 import actions, {_onFail, _onSuccess} from '@store/actions';
-import API from '@utils/api';
-import {hanldeError} from '@utils/handleError';
+import api from '@utils/api';
+import {handleApiError} from '@utils';
 import {put, takeLatest} from 'redux-saga/effects';
 
 function* getSearch(payload) {
   try {
-    const res = yield API.get('getProduct?numshow=12', payload.params);
+    const res = yield api.get('getProduct?numshow=12', payload.params);
     yield put({
       type: _onSuccess(actions.GET_SEARCH_SCREEN),
       data: res.data,
@@ -14,19 +14,19 @@ function* getSearch(payload) {
     });
   } catch (error) {
     yield put({type: _onFail(actions.GET_SEARCH_SCREEN)});
-    hanldeError(error);
+    handleApiError(error);
   }
 }
 function* getSuggestions() {
   try {
-    const res = yield API.get('getSuggestions');
+    const res = yield api.get('getSuggestions');
     yield put({
       type: _onSuccess(actions.GET_SEARCH_SUGGESTIONS),
       data: res.data,
     });
   } catch (error) {
     yield put({type: _onFail(actions.GET_SEARCH_SUGGESTIONS)});
-    hanldeError(error);
+    handleApiError(error);
   }
 }
 
